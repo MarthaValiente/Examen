04672d28e3d44317ffcb2056c59989934aab66cd
MarthaValiente <39814663+MarthaValiente@users.noreply.github.com>
2018-09-20 18:40:34 -0600

    Add files via upload

---
+++ js/my-app.js
@@ -0,0 +1,30 @@
+// Initialize your app
+var myApp = new Framework7({
+	root:'#app',
+	theme: 'md', //ios, md, auto
+    modalTitle: 'Framework7', 	//title for modals
+	panel: {
+    	swipe: 'left',
+  	},
+	cache:false,
+	pushState:false,
+	swipeBackPage:true,
+	material: true,
+	routes: [
+		{
+		path: '/room/',
+    	url: 'room.html',
+    	name: 'room',
+  		},
+	]
+});
+
+// Export selectors engine
+var $$ = Dom7;
+
+
+
+
+
+ 
+
